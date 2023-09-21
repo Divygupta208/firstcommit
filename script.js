8e6aeb1eb92baf3dd137b07985ca94ecf2e38b46
Divy Gupta <divygupta208@gmail.com>
2023-09-21 15:44:41 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const header = document.getElementById('main-header');
+
+header.style.border = "solid 3px #000"
+
+const add = document.getElementsByTagName('h2')
+
+add[0].style.color = "green"
+add[0].style.fontWeight = "bold"
+
