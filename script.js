66e32f0e6d2c491a3517dad14ed937b91b6cccd4
Divy Gupta <divygupta208@gmail.com>
2023-09-22 02:01:02 +0530

    third commit

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ const add = document.getElementsByTagName('h2')
 add[0].style.color = "green"
 add[0].style.fontWeight = "bold"
 
+const list = document.getElementsByClassName('list-group-item')
+
+list[2].style.backgroundColor = "green"
+
+for(let i = 0 ; i<list.length ; i++){
+
+    list[i].style.fontWeight = "bold"
+
+}
