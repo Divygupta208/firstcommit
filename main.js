70009d8b1a76524e5f09e00d53f6895ddc489c64
Divy Gupta <divygupta208@gmail.com>
2023-10-24 14:54:37 +0530

    BookingAppointmentApp

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 var form = document.getElementById('my-form');
 var itemList = document.querySelector('.items');
+var isEditing = false;
 
-
-form.addEventListener('submit' , addItem);
 document.addEventListener('DOMContentLoaded', getUser)
-itemList.addEventListener('click', deleteUser);
+
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  if (isEditing) {
+  
+    updateUser(e);
+  } else {
+   
+    addItem(e);
+  }
+});
+
+
 
  function addItem(e){
   e.preventDefault();
@@ -20,7 +33,7 @@ itemList.addEventListener('click', deleteUser);
   }
   
   axios
-  .post("https://crudcrud.com/api/5ff0ae9021034b45a65797713719872f/userAppointement",myobj)
+  .post("https://crudcrud.com/api/cc61ff98d9b64742a02cc9f9538b6590/userAppointement",myobj)
   .then((res)=>{
    
      showOutput(res.data);
@@ -29,17 +42,12 @@ itemList.addEventListener('click', deleteUser);
     document.body.innerHTML = document.body.innerHTML + "<h4>Something Went Wrong</h4>";
   })
     
-  // editBtn.onclick = ()=> {
-  //   document.getElementById('name').value = myobj.name ;
-  //   document.getElementById('email').value = myobj.e_mail ;
-  //    removeLocal(myobj.e_mail);
-  // }
-
+ 
 
 }
 
 function getUser(){
-  axios.get("https://crudcrud.com/api/5ff0ae9021034b45a65797713719872f/userAppointement")
+  axios.get("https://crudcrud.com/api/cc61ff98d9b64742a02cc9f9538b6590/userAppointement")
   .then((response)=>{
     
     for(var i = 0 ; i<response.data.length ; i++){
@@ -64,7 +72,10 @@ function showOutput(res){
     box2.className = 'box2';
     box1.appendChild(document.createTextNode(`${res.name} - ${res.e_mail} - ${res.phone}`));
     Li.setAttribute("id" ,res._id);
-  
+    Li.setAttribute("name" ,res.name);
+    Li.setAttribute("mail" ,res.e_mail);
+    Li.setAttribute("phone" ,res.phone);
+    
     var deleteBtn = document.createElement('button');
     var editBtn = document.createElement('button');
     deleteBtn.className = 'btn btn-danger btn ms-4 float-right delete';
@@ -79,24 +90,73 @@ function showOutput(res){
   Li.appendChild(box2);
 
   itemList.appendChild(Li);
-    
+  deleteBtn.addEventListener('click', deleteUser);
+  editBtn.addEventListener('click' , editUser);
 
 }
 
 function deleteUser(e){
  
   e.preventDefault();
-  var id = e.target.parentElement.parentElement.getAttribute('id');
+  var ide = e.target.parentElement.parentElement.getAttribute('id');
   itemList.removeChild(e.target.parentElement.parentElement);
   axios
-  .delete(`https://crudcrud.com/api/5ff0ae9021034b45a65797713719872f/userAppointement/${id}`);
+  .delete(`https://crudcrud.com/api/cc61ff98d9b64742a02cc9f9538b6590/userAppointement/${ide}`);
   
 }
 
+
+function editUser(e) {
+  isEditing = true;
+  e.preventDefault();
+   const id = e.target.parentElement.parentElement.getAttribute('id');
+  const name = e.target.parentElement.parentElement.getAttribute('name');
+  const mail = e.target.parentElement.parentElement.getAttribute('mail');
+  const phone = e.target.parentElement.parentElement.getAttribute('phone');
+
+  document.querySelector('#id').value = id;
+  document.querySelector('#name').value = name;
+  document.querySelector('#email').value = mail;
+  document.querySelector('#phone').value = phone;
+
+  itemList.removeChild(e.target.parentElement.parentElement);
+ 
   
+}
+  
+function updateUser(e) {
+  e.preventDefault();
+
+  const id = document.querySelector('#id').value;
+  const updatedName = document.querySelector('#name').value;
+  const updatedEmail = document.querySelector('#email').value;
+  const updatedPhone = document.querySelector('#phone').value;
+
+  const updatedUser = {
+    "name": updatedName,
+    "e_mail": updatedEmail,
+    "phone": updatedPhone,
+  };
 
 
-
+axios
+.put(`https://crudcrud.com/api/cc61ff98d9b64742a02cc9f9538b6590/userAppointement/${id}`, updatedUser)
+.then((res) => {
+  axios
+  .get(`https://crudcrud.com/api/cc61ff98d9b64742a02cc9f9538b6590/userAppointement/${id}`)
+  .then((res)=>{
+    showOutput(res.data);
+  })
+  .catch(err => console.log(err));
+  
+ 
+})
+.catch((err) => {
+  console.error( err);
+});
+  
+isEditing = false;
+}
 
 
 
